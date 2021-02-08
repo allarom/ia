3f4c996b2916478cfdfc301cd187b8657247e77a
Alla Roman <alla.roman@tamedia.ch>
2021-02-08 15:52:28 +0100

    last changes

--- pages/form.js
+++ pages/form.js
@@ -1,7 +1,81 @@
 import styles from './Form.module.css'
-
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Form() {
+
+  const initialForm = {
+    name: '',
+    favSong: '',
+    favDrink: ''
+  }
+
+  const router = useRouter()
+  const contentType = 'application/json'
+  const [errors, setErrors] = useState({})
+  const [message, setMessage] = useState('')
+
+  const [form, setForm] = useState({
+    name: initialForm.name || '',
+    favSong: initialForm.favSong || '',
+    favDrink: initialForm.favDrink || ''
+  })
+
+  const handleChange = (e) => {
+    const target = e.target
+    const value =
+      target.name === 'poddy_trained' ? target.checked : target.value
+    const name = target.name
+
+    setForm({
+      ...form,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const errs = formValidate()
+    if (Object.keys(errs).length === 0) {
+      postData(form)
+    } else {
+      setErrors({ errs })
+    }
+  }
+
+    /* Makes sure pet info is filled for pet name, owner name, species, and image url*/
+    const formValidate = () => {
+      let err = {}
+      if (!form.name) err.name = 'Name is required'
+      if (!form.favDrink) err.favDrink = 'Drink is required'
+      if (!form.favSong) err.favSong = 'Song is required'
+      return err
+    }
+
+  /* The POST method adds a new entry in the mongodb database. */
+  const postData = async (form) => {
+    try {
+      const res = await fetch('/api/users', {
+        method: 'POST',
+        headers: {
+          Accept: contentType,
+          'Content-Type': contentType,
+        },
+        body: JSON.stringify(form),
+      })
+
+      console.log("res", res, form)
+
+      // Throw error with status code in case Fetch API req failed
+      if (!res.ok) {
+        throw new Error(res.status)
+      }
+
+      router.push('/')
+    } catch (error) {
+      setMessage('Failed to add')
+    }
+  }
 
 return <div className={styles.wrapper}>
   <img className={styles.image} src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1024px-NASA_logo.svg.png' />
@@ -10,29 +84,34 @@ return <div className={styles.wrapper}>
 Your courage and self-sacrifice deserves the highest praise. Please enter few details about you, so we can make your life on Mars as comfortable as possible. </p>
   <p className={styles.text} style={{marginBottom: '70px'}}>Sincerely yours,
   Elon Musk</p>
-
-  <label for="name">Name:</label> <br></br>
-  <input type="text" id="name" name="name" required
-       minlength="4" maxlength="20" size="20"/>
+<form onSubmit={handleSubmit}>
+  <label htmlFor="name">Name:</label> <br></br>
+  <input type="text" id="name" name="name" value={form.name}
+      onChange={handleChange} required
+       minLength="4" maxLength="20" size="20"/>
   <br></br>
-  <label for="song">Favorite song:</label> <br></br>
-  <input type="text" id="song" name="song" required
-       minlength="4" maxlength="40" size="20"/>
+  <label htmlFor="favSong">Favorite song:</label> <br></br>
+  <input type="text" id="favSong" name="favSong" value={form.favSong}
+  onChange={handleChange} required minLength="4" maxLength="40" size="20"/>
   <br></br>
-    <label for="drink">Favorite drink:</label> <br></br>
-  <input type="text" id="drink" name="drink" required
-       minlength="4" maxlength="40" size="20"/>
+    <label htmlFor="favDrink">Favorite drink:</label> <br></br>
+  <input type="text" id="favDrink" name="favDrink" value={form.favDrink}
+      onChange={handleChange} required
+       minLength="4" maxLength="40" size="20"/>
   <br></br>
-      <label for="degree">Your degree:</label> <br></br>
+      {/* <label htmlFor="degree">Your degree:</label> <br></br>
   <input type="text" id="degree" name="degree" required
-       minlength="4" maxlength="40" size="20"/>
+       minLength="4" maxLength="40" size="20"/>
   <br></br>
-        <label for="tasks">Your current tasks:</label> <br></br>
+        <label htmlFor="tasks">Your current tasks:</label> <br></br>
   <input type="text" id="tasks" name="tasks" required
-       minlength="4" maxlength="40" size="20"/>
+       minLength="4" maxLength="40" size="20"/>
   <br></br>
-          <label for="skills">Your unique skills or strengths:</label> <br></br>
+          <label htmlFor="skills">Your unique skills or strengths:</label> <br></br>
   <input type="text" id="skills" name="skills" required
-       minlength="4" maxlength="40" size="20"/>
+       minLength="4" maxLength="40" size="20"/>
+  <br></br> */}
+  <input type="submit" value="Submit" ></input>
+  </form>
   </div>
 }
